6203710cfe959f09e872f579c82f8069ab85bcf5
davie72 <54401884+davie72@users.noreply.github.com>
2022-01-13 10:53:36 +0000

    Security Services added to account customizations

--- sec-services/terraform/sec-services.tf
+++ sec-services/terraform/sec-services.tf
@@ -2,39 +2,32 @@ data "aws_caller_identity" "current" {}
 
 # Enable GuardDuty
 
-resource "aws_organizations_organization" "sec-tooling" {
-  aws_service_access_principals = ["guardduty.amazonaws.com", "securityhub.amazonaws.com"]
-  feature_set                   = "ALL"
-}
+resource "aws_guardduty_detector" "sec-tooling2" {}
 
-resource "aws_guardduty_detector" "sec-tooling" {}
-
-resource "aws_guardduty_organization_admin_account" "sec-tooling" {
-  depends_on = [aws_organizations_organization.sec-tooling]
+resource "aws_guardduty_organization_admin_account" "sec-tooling2" {
+  depends_on = [aws_organizations_organization.sec-tooling2]
   admin_account_id = data.aws_caller_identity.current.account_id
 }
 
 #  Enable Macie
 
-resource "aws_macie2_account" "sec-tooling" {}
+resource "aws_macie2_account" "member-acc" {}
 
-resource "aws_macie2_organization_admin_account" "sec-tooling" {
+resource "aws_macie2_organization_admin_account" "sec-tooling2" {
   admin_account_id = data.aws_caller_identity.current.account_id
-  depends_on       = [aws_macie2_account.sec-tooling]
+  depends_on       = [aws_macie2_account.member-acc]
 }
 
 # Enable SecurityHub
 
-resource "aws_securityhub_account" "sec-tooling" {}
-
-resource "aws_securityhub_organization_admin_account" "sec-tooling" {
-  depends_on = [aws_organizations_organization.sec-tooling]
+resource "aws_securityhub_organization_admin_account" "sec-tooling2" {
+  depends_on = [aws_organizations_organization.sec-tooling2]
   admin_account_id = data.aws_caller_identity.current.account_id
 }
 
 # Auto enable security hub in organization member accounts
 
-resource "aws_securityhub_organization_configuration" "sec-tooling" {
+resource "aws_securityhub_organization_configuration" "sec-tooling2" {
   auto_enable = true
 }
 
